ef7941f607e267cd418e0000c90d7e4a18bd4dd3
Luisa Scavo <luisascavo@gmail.com>
2023-04-13 17:11:23 -0400

    initial commit w dealing machanics

---
+++ index.js
@@ -0,0 +1,34 @@
+const game = document.getElementById('game');
+
+const symbols = ["★","▲","●","✿","𓆙","𓅼","𓆏","𓆧"];
+let gameCards = randomizeArray();
+
+function randomizeArray() {
+ let gameBoard = symbols.concat(symbols);
+ 
+  // randomize using Schwartzian transform
+  return gameBoard = gameBoard
+    .map(value => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value);
+
+}
+
+function dealCards(){
+  gameCards.forEach(c => buildCard(c) )
+}
+
+function buildCard(sym){
+  const card = document.createElement('div');
+  const cardText = document.createTextNode(sym);
+  card.appendChild(cardText);
+  card.classList.add('card');
+  // temp
+  game.appendChild(card);
+}
+
+function isMatch(x,y){
+return x == y;
+}
+
+randomizeArray();
